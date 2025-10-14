f075a737e434c8e1c27a0532714d0a3e5ca8e786
Robin Kertels <robin.kertels@gmail.com>
2025-10-15 01:32:25 +0200

    newer stuff

--- sm3/sm3_io_map.cpp
+++ sm3/sm3_io_map.cpp
@@ -18,47 +18,12 @@ IoMap::~IoMap() {
 
 void IoMap::initialize(ir::Builder& builder) {
   const ShaderInfo& info = m_converter.getShaderInfo();
+  IoSemanticMap& semanticMap = m_converter.getSemanticMap();
 
   if (info.getVersion().first >= 3u) {
-    // Emit scratch arrays that we copy input/output registers in and out
-    // to allow relative addressing
+    // Emit functions that pick a register using
+    // a switch statement to allow relative addressing
 
-    // Inputs
-    uint32_t inputArraySize = info.getType() == ShaderType::eVertex ? 16u : 10u;
-
-    auto inputArrayType = ir::Type(ir::ScalarType::eF32, 4u)
-      .addArrayDimension(inputArraySize);
-
-    auto inputDeclaration = ir::Op(ir::OpCode::eDclScratch, inputArrayType)
-      .addOperand(m_converter.getEntryPoint());
-
-    auto inputVar = m_inputRegisterArray.emplace();
-    inputVar.baseType = inputArrayType;
-    inputVar.baseDef = builder.add(std::move(inputDeclaration));
-
-    if (m_converter.m_options.includeDebugNames) {
-      auto name = m_converter.makeRegisterDebugName(RegisterType::eInput, 0, WriteMask(ComponentBit::eAll));
-      builder.add(ir::Op::DebugName(inputVar.baseDef, name.c_str()));
-    }
-
-    // Outputs
-    if (info.getType() == ShaderType::eVertex) {
-      // PS Outputs don't support relative addressing
-      auto outputArrayType = ir::Type(ir::ScalarType::eF32, 4u)
-        .addArrayDimension(12u);
-
-      auto outputDeclaration = ir::Op(ir::OpCode::eDclScratch, inputArrayType)
-        .addOperand(m_converter.getEntryPoint());
-
-      auto outputVar = m_outputRegisterArray.emplace();
-      outputVar.baseType = inputArrayType;
-      outputVar.baseDef = builder.add(std::move(outputDeclaration));
-
-      if (m_converter.m_options.includeDebugNames) {
-        auto name = m_converter.makeRegisterDebugName(RegisterType::eOutput, 0, WriteMask(ComponentBit::eAll));
-        builder.add(ir::Op::DebugName(outputVar.baseDef, name.c_str()));
-      }
-    }
   } else {
     // SM 1 & 2 have fixed VS output and PS input registers
     // that do not get explicitly declared.
@@ -70,6 +35,31 @@ void IoMap::initialize(ir::Builder& builder) {
 
     ir::Type type(ir::ScalarType::eF32, 4u);
 
+    // Normal
+    {
+      auto declaration = ir::Op(opCode, type)
+        .addOperand(m_converter.getEntryPoint())
+        .addOperand(0u)
+        .addOperand(0u);
+
+      auto& mapping = m_variables.emplace_back();
+      mapping.semantic = { SemanticUsage::eNormal, 0u };
+      mapping.registerIndex = semanticMap.getIoLocation(mapping.semantic);
+      mapping.componentMask = WriteMask(ComponentBit::eAll);
+      mapping.baseType = declaration.getType();
+      mapping.baseDef = builder.add(std::move(declaration));
+
+      emitDebugName(
+        builder,
+        mapping.baseDef,
+        RegisterType::eRasterizerOut,
+        3,
+        WriteMask(ComponentBit::eAll),
+        mapping.semantic,
+        isInput
+      );
+    }
+
     // Texture coords
     for (uint32_t i = 0u; i < SM2TexCoordCount; i++) {
       auto declaration = ir::Op(opCode, type)
@@ -78,8 +68,8 @@ void IoMap::initialize(ir::Builder& builder) {
         .addOperand(0u);
 
       auto& mapping = m_variables.emplace_back();
-      mapping.semanticUsage = SemanticUsage::eTexCoord;
-      mapping.semanticIndex = i;
+      mapping.semantic = { SemanticUsage::eTexCoord, i };
+      mapping.registerIndex = semanticMap.getIoLocation(mapping.semantic);
       mapping.componentMask = WriteMask(ComponentBit::eAll);
       mapping.baseType = declaration.getType();
       mapping.baseDef = builder.add(std::move(declaration));
@@ -90,8 +80,8 @@ void IoMap::initialize(ir::Builder& builder) {
         isInput ? RegisterType::ePixelTexCoord : RegisterType::eTexCoordOut,
         i,
         WriteMask(ComponentBit::eAll),
-        SemanticUsage::eTexCoord,
-        i
+        mapping.semantic,
+        isInput
       );
     }
 
@@ -103,8 +93,8 @@ void IoMap::initialize(ir::Builder& builder) {
         .addOperand(0u);
 
       auto& mapping = m_variables.emplace_back();
-      mapping.semanticUsage = SemanticUsage::eColor;
-      mapping.semanticIndex = i;
+      mapping.semantic = { SemanticUsage::eColor, i };
+      mapping.registerIndex = semanticMap.getIoLocation(mapping.semantic);
       mapping.componentMask = WriteMask(ComponentBit::eAll);
       mapping.baseType = declaration.getType();
       mapping.baseDef = builder.add(std::move(declaration));
@@ -115,8 +105,33 @@ void IoMap::initialize(ir::Builder& builder) {
         isInput ? RegisterType::eInput : RegisterType::eColorOut,
         i,
         WriteMask(ComponentBit::eAll),
-        SemanticUsage::eColor,
-        i
+        mapping.semantic,
+        isInput
+      );
+    }
+
+    // Fog
+    {
+      auto declaration = ir::Op(opCode, type)
+        .addOperand(m_converter.getEntryPoint())
+        .addOperand(0u)
+        .addOperand(0u);
+
+      auto& mapping = m_variables.emplace_back();
+      mapping.semantic = { SemanticUsage::eFog, 0u };
+      mapping.registerIndex = semanticMap.getIoLocation(mapping.semantic);
+      mapping.componentMask = WriteMask(ComponentBit::eAll);
+      mapping.baseType = declaration.getType();
+      mapping.baseDef = builder.add(std::move(declaration));
+
+      emitDebugName(
+        builder,
+        mapping.baseDef,
+        RegisterType::eRasterizerOut,
+        uint32_t(RasterizerOutIndex::eRasterOutFog),
+        WriteMask(ComponentBit::eAll),
+        mapping.semantic,
+        isInput
       );
     }
   }
@@ -131,17 +146,16 @@ bool IoMap::handleDclIoVar(ir::Builder& builder, const Instruction& op) {
     ? ir::OpCode::eDclInput
     : ir::OpCode::eDclOutput;
 
-  WriteMask componentMask = dst.getWriteMask();
+  WriteMask componentMask = dst.getWriteMask(m_converter.getShaderInfo());
   if (m_converter.getShaderInfo().getVersion().first < 3) {
     componentMask = WriteMask(ComponentBit::eAll);
   }
 
-  SemanticUsage semanticUsage = dcl.getSemanticUsage();
-  uint32_t semanticIndex = dcl.getSemanticIndex();
+  Semantic semantic = { dcl.getSemanticUsage(), dcl.getSemanticIndex() };
 
   bool foundExisting = false;
   for (auto& entry : m_variables) {
-    if (entry.semanticUsage == semanticUsage && entry.semanticIndex == semanticIndex) {
+    if (entry.semantic == semantic) {
       foundExisting = true;
       break;
     }
@@ -156,8 +170,8 @@ bool IoMap::handleDclIoVar(ir::Builder& builder, const Instruction& op) {
     .addOperand(util::tzcnt(uint8_t(componentMask)));
 
   auto& mapping = m_variables.emplace_back();
-  mapping.semanticUsage = semanticUsage;
-  mapping.semanticIndex = semanticIndex;
+  mapping.semantic = semantic;
+  mapping.registerIndex = m_converter.getSemanticMap().getIoLocation(semantic);
   mapping.componentMask = componentMask;
   mapping.baseType = declaration.getType();
   mapping.baseDef = builder.add(std::move(declaration));
@@ -167,9 +181,9 @@ bool IoMap::handleDclIoVar(ir::Builder& builder, const Instruction& op) {
     mapping.baseDef,
     dst.getRegisterType(),
     dst.getIndex(),
-    dst.getWriteMask(),
-    dcl.getSemanticUsage(),
-    dcl.getSemanticIndex()
+    dst.getWriteMask(m_converter.getShaderInfo()),
+    mapping.semantic,
+    isInput
   );
 
   return true;
@@ -201,68 +215,69 @@ void IoMap::emitDebugName(
   RegisterType registerType,
   uint32_t registerIndex,
   WriteMask writeMask,
-  SemanticUsage semanticUsage,
-  uint32_t semanticIndex) const {
+  Semantic semantic,
+  bool isInput) const {
   if (!m_converter.m_options.includeDebugNames)
     return;
 
   std::string name;
-
-  if ((registerType == RegisterType::eOutput && m_converter.getShaderInfo().getVersion().first >= 3)
-    || registerType == RegisterType::eInput) {
-    switch (semanticUsage) {
-      case SemanticUsage::ePosition:
-        name = "position" + std::to_string(semanticIndex);
-        break;
-      case SemanticUsage::eBlendWeight:
-        name = "weight";
-        break;
-      case SemanticUsage::eBlendIndices:
-        name = "blend";
-        break;
-      case SemanticUsage::eNormal:
-        name = "normal" + std::to_string(semanticIndex);
-        break;
-      case SemanticUsage::ePointSize:
-        name = "psize";
-        break;
-      case SemanticUsage::eTexCoord:
-        name = "texcoord" + std::to_string(semanticIndex);
-        break;
-      case SemanticUsage::eTangent:
-        name = "tangent";
-        break;
-      case SemanticUsage::eBinormal:
-        name = "binormal";
-        break;
-      case SemanticUsage::eTessFactor:
-        name = "tessfactor";
-        break;
-      case SemanticUsage::ePositionT:
-        name = "positiont";
-        break;
-      case SemanticUsage::eColor:
-        if (semanticIndex == 0) {
-          name = "color";
-        } else {
-          name = "specular" + std::to_string(semanticIndex - 1u);
-        }
-        break;
-      case SemanticUsage::eFog:
-        name = "fog";
-        break;
-      case SemanticUsage::eDepth:
-        name = "depth";
-        break;
-      case SemanticUsage::eSample:
-        name = "sample" + std::to_string(semanticIndex);
-        break;
-      default:
-        name = "unknown";
-        break;
-    }
-  } else {
-    name = m_converter.makeRegisterDebugName(registerType, registerIndex, writeMask);
+  if (semantic.usage != SemanticUsage::eNormal
+    || (isInput && registerType == RegisterType::eRasterizerOut)
+    || (!isInput && registerType == RegisterType::eMiscType)) {
+    // There is no register type for normals, it's only emitted for FF emulation.
+    // The other exceptions either only have input only or output only registers.
+    name = m_converter.makeRegisterDebugName(registerType, registerIndex, writeMask) + "_";
+  }
+  switch (semantic.usage) {
+    case SemanticUsage::ePosition:
+      name += "position" + std::to_string(semantic.index);
+      break;
+    case SemanticUsage::eBlendWeight:
+      name += "weight";
+      break;
+    case SemanticUsage::eBlendIndices:
+      name += "blend";
+      break;
+    case SemanticUsage::eNormal:
+      name += "normal" + std::to_string(semantic.index);
+      break;
+    case SemanticUsage::ePointSize:
+      name += "psize";
+      break;
+    case SemanticUsage::eTexCoord:
+      name += "texcoord" + std::to_string(semantic.index);
+      break;
+    case SemanticUsage::eTangent:
+      name += "tangent";
+      break;
+    case SemanticUsage::eBinormal:
+      name += "binormal";
+      break;
+    case SemanticUsage::eTessFactor:
+      name += "tessfactor";
+      break;
+    case SemanticUsage::ePositionT:
+      name += "positiont";
+      break;
+    case SemanticUsage::eColor:
+      if (semantic.index == 0) {
+        name += "color";
+      } else {
+        name += "specular" + std::to_string(semantic.index - 1u);
+      }
+      break;
+    case SemanticUsage::eFog:
+      name += "fog";
+      break;
+    case SemanticUsage::eDepth:
+      name += "depth";
+      break;
+    case SemanticUsage::eSample:
+      name += "sample" + std::to_string(semantic.index);
+      break;
+    default:
+      name += "unknown";
+      break;
   }
 
   builder.add(ir::Op::DebugName(def, name.c_str()));
